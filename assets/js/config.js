ba4127e2143425a83db62ba0821e7107ac993574
cyber.sabil <67240883+cybersabil@users.noreply.github.com>
2026-08-07 14:28:36 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,1 @@
-window.DEVICE_STATUS_CONFIG=Object.freeze({apiBaseUrl:"https://device-status-backend.onrender.com",healthTimeoutMs:120000,apiTimeoutMs:180000,maxCombinationsPerBatch:40,retryCount:1,rowsPerPage:100});
+window.DEVICE_STATUS_CONFIG=Object.freeze({apiBaseUrl:"https://cybersabil-device.xubi.org",healthTimeoutMs:120000,apiTimeoutMs:180000,maxCombinationsPerBatch:40,retryCount:1,rowsPerPage:100});
